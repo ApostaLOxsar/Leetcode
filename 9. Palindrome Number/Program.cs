ffff5bf1ef297630e20f9aa2d1bf5f7ccb5205f0
Stepan <stepanmaslov@gmail.com>
2023-04-06 11:35:49 +0300

    add task 9

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,16 @@
+﻿//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+int x = 1210;
+Console.WriteLine(IsPalindrome(x));
+bool IsPalindrome(int x)
+{
+    if (x < 0) return false;
+    else if (x < 10) return true;
+    else
+    {
+        string massiv = x.ToString();
+        for (int i = 0; i < massiv.Length / 2; i++)
+            if (massiv[i] != massiv[massiv.Length - 1 - i]) return false;
+        return true;
+    }
+}
